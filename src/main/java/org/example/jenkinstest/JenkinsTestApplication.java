352dd29aa8aaff20ef5957d8a6f0b7a0f1d6258b
pankaj pathak <32pankaj@gmail.com>
2024-03-15 01:19:12 +0530

    Merge remote-tracking branch 'origin/master'

--- src/main/java/org/example/jenkinstest/JenkinsTestApplication.java
+++ src/main/java/org/example/jenkinstest/JenkinsTestApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JenkinsTestApplication {
 
+
+
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsTestApplication.class, args);
 
