f0a6b23e594e9589d24401972a43a8d44862c57b
pankaj pathak <32pankaj@gmail.com>
2024-03-15 01:14:07 +0530

    Initial commit

--- src/main/java/org/example/jenkinstest/JenkinsTestApplication.java
+++ src/main/java/org/example/jenkinstest/JenkinsTestApplication.java
@@ -1,6 +1,6 @@
 package org.example.jenkinstest;
 
-<<<<<<< HEAD
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,15 +9,6 @@ public class JenkinsTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsTestApplication.class, args);
-=======
 
-public class JenkinsTestApplication {
-
-	public static void main(String[] args) {
-
->>>>>>> 338fb5c (Initial commit)
-
-		System.out.println("This is jenkins test");
 	}
-
 }
