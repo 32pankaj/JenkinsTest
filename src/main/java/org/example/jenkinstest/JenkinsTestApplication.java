5d1e218ae88b62f5c64a3e5a723ef9e6f3424380
pankaj pathak <32pankaj@gmail.com>
2024-03-16 01:05:45 +0530

    Merge remote-tracking branch 'origin/master'

--- src/main/java/org/example/jenkinstest/JenkinsTestApplication.java
+++ src/main/java/org/example/jenkinstest/JenkinsTestApplication.java
@@ -1,6 +1,10 @@
 package org.example.jenkinstest;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class JenkinsTestApplication {
 
 
@@ -12,5 +16,35 @@ public class JenkinsTestApplication {
 
 		System.out.println("This is is  ");
 
+		List<Product> productsList;
+        productsList = new ArrayList<Product>();
+        //Adding Products
+		productsList.add(new Product(1,"HP Laptop",25000f));
+		productsList.add(new Product(2,"Dell Laptop",30000f));
+		productsList.add(new Product(3,"Lenevo Laptop",28000f));
+		productsList.add(new Product(4,"Sony Laptop",28000f));
+		productsList.add(new Product(5,"Apple Laptop",90000f));
+
+		Float v = productsList.stream().filter(p -> p.price > 25000)
+				.map(p -> p.price)
+				.reduce(1000.0f,(price, sum) -> sum + price);
+		System.out.println(v);
+
+		Optional<Float> reduce = productsList.stream()
+				.map(p -> p.price)
+				.reduce(Float::sum);
+		System.out.println(reduce.get());
+
+
+	}
+}
+class Product{
+	int id;
+	String name;
+	float price;
+	public Product(int id, String name, float price) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
 	}
 }
